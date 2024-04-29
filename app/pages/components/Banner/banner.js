2a1763d958e031b31eb3a556fc1683eb0e006c0a
zaryabzaheer8888 <muhammad.zaryab@arhamsoft.org>
2024-04-29 16:40:16 +0500

    remove space

--- app/pages/components/Banner/banner.js
+++ app/pages/components/Banner/banner.js
@@ -3,7 +3,6 @@ import { Images } from '../../assets/assets'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
-
 function Banner() {
 
     return (
