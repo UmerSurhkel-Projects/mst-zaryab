df260fd2a4353eb333859ea7425ffd9402ff1db4
zaryabzaheer8888 <muhammad.zaryab@arhamsoft.org>
2024-04-30 11:47:54 +0500

    dynamically sitemap

--- generateSitemap.js
+++ generateSitemap.js
@@ -1,18 +1,16 @@
-
 const { SitemapStream, streamToPromise } = require('sitemap');
-const { createGzip } = require('zlib');
 const fs = require('fs');
 const path = require('path');
 
 // Import all pages
 const { getAllPages } = require('./app/utils/pageUtils'); // Import your utility function to get all pages
+console.log(getAllPages, "getall pages...");
 
 // Function to generate sitemap XML
 async function generateSitemap() {
   const pages = await getAllPages(); // Retrieve all pages using your utility function
 
   const stream = new SitemapStream({ hostname: 'https://mst-zaryab.vercel.app/' }); // Replace with your domain
-  const pipeline = stream.pipe(createGzip());
 
   pages.forEach((page) => {
     stream.write({ url: page.url, changefreq: 'weekly', priority: 0.7 });
@@ -20,10 +18,11 @@ async function generateSitemap() {
 
   stream.end();
 
-  return streamToPromise(pipeline).then((data) => data.toString());
+  return streamToPromise(stream).then((data) => data.toString()); // Return the sitemap without gzip compression
 }
 
 // Generate sitemap and save to file
 generateSitemap().then((sitemap) => {
-  fs.writeFileSync(path.join(process.cwd(), 'public', 'sitemap.xml.gz'), sitemap); // Save the sitemap in the public directory
+  fs.writeFileSync(path.join(process.cwd(), 'public', 'sitemap.xml'), sitemap); 
 });
+
