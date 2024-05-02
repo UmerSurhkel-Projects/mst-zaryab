41cce7565809bd1b1ea2f0084b7b2c187b17ba85
zaryabzaheer8888 <muhammad.zaryab@arhamsoft.org>
2024-05-02 16:45:17 +0500

    structured data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,111 +1,87 @@
-// 'use client'
-// import { Inter } from "next/font/google";
-// import Head from "next/head";
-// import { usePathname } from 'next/navigation';
-// import { DefaultSeo } from "next-seo";
-// import '../public/assets/css/globals.css';
-
-// const inter = Inter({ subsets: ["latin"] });
-
-// const RootLayout = ({children, title, description}) => {
-//   const metadata = {
-//     title: title,
-//     description: description,
-//   };
-//   const pathname = usePathname();
-//   const canonicalUrl = `https://mst-zaryab.vercel.app${pathname}`;
-//   const imageUrl = "https://mst-zaryab.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fheader-logo.79c23562.png&w=256&q=75";
-  
-//   const structuredData = {
-//     "@context": "http://schema.org",
-//     "@type": "Organization",
-//     "name": "AssetTrack",
-//     "url": canonicalUrl,
-//     "logo": imageUrl,
-//   };
-
-//   return (
-//     <html lang="en">
-//       <Head>
-//         <title>{title || metadata.title}</title>
-//         <meta name="description" content={description || metadata.description} />
-//         <DefaultSeo canonical={canonicalUrl} />
-//         <meta property="og:title" content={title || metadata.title} />
-//         <meta property="og:type" content="website" />
-//         <meta property="og:url" content={canonicalUrl} />
-//         <meta property="og:image" content={imageUrl} />
-//         <meta property="og:description" content={description || metadata.description} />
-//         <script
-//           type="application/ld+json"
-//           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
-//         />
-//       </Head>
-//       <body className={inter.className}>
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
-
-// export default RootLayout;
-
-// app/layout.js
-
-
-'use client'
-import { ReactNode } from 'react';
-import { Inter } from "next/font/google";
-import Head from "next/head";
-import { usePathname } from 'next/navigation';
-import { DefaultSeo } from "next-seo";
 import '../public/assets/css/globals.css';
+import type { Metadata } from "next";
 
-const inter = Inter({ subsets: ["latin"] });
+export const metadata: Metadata = {
+  metadataBase: new URL("https://mst-zaryab.vercel.app/"),
+  title: {
+    default: "AssetTrack",
+    template: `$%s - AssetTrack`,
+  },
+  description:
+    "AssetTrack is a comprehensive asset management solution for businesses.",
+  keywords: [
+    "AssetTrack",
+    "asset management",
+    "inventory management",
+    "business assets",
+    "asset tracking",
+  ],
+  authors: [
+    {
+      name: "Assettrack",
+      url: "https://mst-zaryab.vercel.app/",
+    },
+  ],
+  creator: "YourName",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://mst-zaryab.vercel.app/",
+    title: "AssetTrack",
+    description:
+      "AssetTrack is a comprehensive asset management solution for businesses.",
+    siteName: "AssetTrack",
+    images: [
+      {
+        url: `https://mst-zaryab.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fheader-logo.79c23562.png&w=256&q=75g`,
+        width: 1200,
+        height: 715,
+        alt: "AssetTrack Logo",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "AssetTrack",
+    description:
+      "AssetTrack is a comprehensive asset management solution for businesses.",
+    creator: "@yourtwitterhandle",
+    images: [`https://mst-zaryab.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fheader-logo.79c23562.png&w=256&q=75`],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+const structuredData = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "AssetTrack",
+  "url": "https://mst-zaryab.vercel.app/",
+  "logo": "https://mst-zaryab.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fheader-logo.79c23562.png&w=256&q=75",
+  "sameAs": [
+    "https://twitter.com/yourtwitterhandle",
+    "https://www.linkedin.com/company/yourcompanyname",
+    "https://www.facebook.com/yourfacebookpage",
+  ]
+};
 
-interface RootLayoutProps {
-  children: ReactNode;
-  title: string;
-  description: string;
-}
-
-const RootLayout: React.FC<RootLayoutProps> = ({ children, title, description }) => {
-  const metadata = {
-    title: title,
-    description: description,
-  };
-  const pathname = usePathname();
-  const canonicalUrl = `https://mst-zaryab.vercel.app${pathname}`;
-  const imageUrl = "https://mst-zaryab.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fheader-logo.79c23562.png&w=256&q=75";
-  
-  const structuredData = {
-    "@context": "http://schema.org",
-    "@type": "Organization",
-    "name": "AssetTrack",
-    "url": canonicalUrl,
-    "logo": imageUrl,
-  };
-
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <Head>
-        <title>{title || metadata.title}</title>
-        <meta name="description" content={description || metadata.description} />
-        <DefaultSeo canonical={canonicalUrl} />
-        <meta property="og:title" content={title || metadata.title} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={canonicalUrl} />
-        <meta property="og:image" content={imageUrl} />
-        <meta property="og:description" content={description || metadata.description} />
+     <head>
+     <link rel="canonical" href={metadata?.metadataBase?.href} />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
         />
-      </Head>
-      <body className={inter.className}>
+      </head>
+      <body>
         {children}
       </body>
     </html>
   );
 }
-
-export default RootLayout;
